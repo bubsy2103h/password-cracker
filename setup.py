8bffe3e84108ff2834e24fa3a858f105c4ff6705
Henry Scheible <henry.scheible@gmail.com>
2018-08-14 12:39:04 -0500

    create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+    name='password-cracker',
+    packages=['password-cracker'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
